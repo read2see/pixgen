a61243472c93c19d1988e8168932de6cc46892b4
Husain Habib <habib.h.e@outlook.com>
2026-03-31 19:30:46 +0300

    setup project structure, add dependencies, set up initial dev profile

---
+++ src/main/java/com/ga/pixgen/PixgenApplication.java
@@ -0,0 +1,13 @@
+package com.ga.pixgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/pixgen/PixgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.pixgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
