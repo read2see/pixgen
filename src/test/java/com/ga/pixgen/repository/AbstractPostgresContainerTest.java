2ca3f095df04351b73902fe0bdaf5bd9370426b3
Husain Habib <habib.h.e@outlook.com>
2026-04-29 21:37:23 +0300

    test(persistence): use shared postgres test profile

--- src/test/java/com/ga/pixgen/repository/AbstractPostgresContainerTest.java
+++ src/test/java/com/ga/pixgen/repository/AbstractPostgresContainerTest.java
@@ -2,18 +2,10 @@ package com.ga.pixgen.repository;
 
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers(disabledWithoutDocker = true)
+@ActiveProfiles("test")
 public abstract class AbstractPostgresContainerTest {
-
-    @Container
-    @ServiceConnection
-    static final PostgreSQLContainer<?> POSTGRES =
-            new PostgreSQLContainer<>("postgres:16-alpine");
 }
